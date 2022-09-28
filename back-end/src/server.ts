6f1387d65c4c0a960debab79915dae35fa06c61d
Chrystian Silva <chrystian100@hotmail.com>
2022-09-28 00:40:58 -0300

    Added PostgreSQL base connection

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,16 @@
 import app from "./app";
+import pool from './dbconfig/dbconnector';
+
 
 /**
  * Start Express server.
  */
 const server = app.listen(app.get("port"), () => {
+  pool.connect(function (err, client, done) {
+    if (err) throw new Error(err.message);
+    console.log('Connected');
+  });
+
   console.log(
     "  App is running at http://localhost:%d in %s mode",
     app.get("port"),
