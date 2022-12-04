13305fa58c5e03cde63c3b2555e15ec876c88816
Carlos Guilherme Felismino Pedroni <calosguilherme@hotmail.com>
2022-12-04 14:41:37 -0300

    trazendo as atualizacoes do outro projeto

--- front-end/src/app/model/registroMovimentacao.model.ts
+++
@@ -1,8 +0,0 @@
-import { Entidade } from "./entidade.model";
-
-export class RegistroMovimentacao extends Entidade {
-    dataMovimentacao?: Date
-    localOrigem?: String
-    localDestino?: String
-    status?: String
-}
